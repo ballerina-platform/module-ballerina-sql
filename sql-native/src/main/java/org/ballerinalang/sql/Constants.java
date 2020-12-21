205e04c11261cf20df271ae5b3576dee6af76a97
Niveathika <niveathika@wso2.com>
2020-12-21 19:20:09 +0530

    Bump record version in constants

--- sql-native/src/main/java/org/ballerinalang/sql/Constants.java
+++ sql-native/src/main/java/org/ballerinalang/sql/Constants.java
@@ -31,7 +31,7 @@ import static io.ballerina.runtime.api.utils.StringUtils.fromString;
 public final class Constants {
 
     public static final String CONNECTOR_NAME = "SQLClientConnector";
-    public static final Module SQL_PACKAGE_ID = new Module("ballerina", "sql", "0.5.3");
+    public static final Module SQL_PACKAGE_ID = new Module("ballerina", "sql", "0.5.4");
     public static final String DATABASE_CLIENT = "Client";
     public static final String SQL_CONNECTOR_TRANSACTION_ID = "sql-transaction-id";
 
