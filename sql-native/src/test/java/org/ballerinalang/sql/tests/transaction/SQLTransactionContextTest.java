ecd8fee3b5e49da00c3508f7cb32c3b2fcba2756
lahirusamith <lahirusmp@gmail.com>
2021-07-05 09:48:41 +0530

    Add PoolKeyTest test cases

--- sql-native/src/test/java/org/ballerinalang/sql/tests/transaction/SQLTransactionContextTest.java
+++ sql-native/src/test/java/org/ballerinalang/sql/tests/transaction/SQLTransactionContextTest.java
@@ -18,34 +18,15 @@
 
 package org.ballerinalang.sql.tests.transaction;
 
+import org.ballerinalang.sql.tests.TestUtils;
 import org.ballerinalang.sql.transaction.SQLTransactionContext;
 import org.junit.jupiter.api.Test;
 
-import java.sql.Array;
-import java.sql.Blob;
-import java.sql.CallableStatement;
-import java.sql.Clob;
 import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.NClob;
-import java.sql.PreparedStatement;
-import java.sql.SQLClientInfoException;
-import java.sql.SQLException;
-import java.sql.SQLWarning;
-import java.sql.SQLXML;
-import java.sql.Savepoint;
-import java.sql.Statement;
-import java.sql.Struct;
-import java.util.Map;
-import java.util.Properties;
-import java.util.concurrent.Executor;
 
-import javax.transaction.xa.XAException;
 import javax.transaction.xa.XAResource;
-import javax.transaction.xa.Xid;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 
 /**
  * SQLTransactionContext class test.
@@ -53,337 +34,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  * @since 0.6.0-beta.2
  */
 public class SQLTransactionContextTest {
-    Connection connection = new Connection() {
-        @Override
-        public Statement createStatement() throws SQLException {
-            return null;
-        }
-
-        @Override
-        public PreparedStatement prepareStatement(String sql) throws SQLException {
-            return null;
-        }
-
-        @Override
-        public CallableStatement prepareCall(String sql) throws SQLException {
-            return null;
-        }
-
-        @Override
-        public String nativeSQL(String sql) throws SQLException {
-            return null;
-        }
-
-        @Override
-        public void setAutoCommit(boolean autoCommit) throws SQLException {
-
-        }
-
-        @Override
-        public boolean getAutoCommit() throws SQLException {
-            return false;
-        }
-
-        @Override
-        public void commit() throws SQLException {
-            throw new SQLException("Commit Error");
-        }
-
-        @Override
-        public void rollback() throws SQLException {
-            throw new SQLException("Rollback Error");
-        }
-
-        @Override
-        public void close() throws SQLException {
-            throw new SQLException("Close Error");
-        }
-
-        @Override
-        public boolean isClosed() throws SQLException {
-            return false;
-        }
-
-        @Override
-        public DatabaseMetaData getMetaData() throws SQLException {
-            return null;
-        }
-
-        @Override
-        public void setReadOnly(boolean readOnly) throws SQLException {
-
-        }
-
-        @Override
-        public boolean isReadOnly() throws SQLException {
-            return false;
-        }
-
-        @Override
-        public void setCatalog(String catalog) throws SQLException {
-
-        }
-
-        @Override
-        public String getCatalog() throws SQLException {
-            return null;
-        }
-
-        @Override
-        public void setTransactionIsolation(int level) throws SQLException {
-
-        }
-
-        @Override
-        public int getTransactionIsolation() throws SQLException {
-            return 0;
-        }
-
-        @Override
-        public SQLWarning getWarnings() throws SQLException {
-            return null;
-        }
-
-        @Override
-        public void clearWarnings() throws SQLException {
-
-        }
-
-        @Override
-        public Statement createStatement(int resultSetType, int resultSetConcurrency) throws SQLException {
-            return null;
-        }
-
-        @Override
-        public PreparedStatement prepareStatement(String sql, int resultSetType, int resultSetConcurrency)
-                throws SQLException {
-            return null;
-        }
-
-        @Override
-        public CallableStatement prepareCall(String sql, int resultSetType, int resultSetConcurrency)
-                throws SQLException {
-            return null;
-        }
-
-        @Override
-        public Map<String, Class<?>> getTypeMap() throws SQLException {
-            return null;
-        }
-
-        @Override
-        public void setTypeMap(Map<String, Class<?>> map) throws SQLException {
-
-        }
-
-        @Override
-        public void setHoldability(int holdability) throws SQLException {
-
-        }
-
-        @Override
-        public int getHoldability() throws SQLException {
-            return 0;
-        }
-
-        @Override
-        public Savepoint setSavepoint() throws SQLException {
-            return null;
-        }
-
-        @Override
-        public Savepoint setSavepoint(String name) throws SQLException {
-            return null;
-        }
-
-        @Override
-        public void rollback(Savepoint savepoint) throws SQLException {
-
-        }
-
-        @Override
-        public void releaseSavepoint(Savepoint savepoint) throws SQLException {
-
-        }
-
-        @Override
-        public Statement createStatement(int resultSetType, int resultSetConcurrency, int resultSetHoldability)
-                throws SQLException {
-            return null;
-        }
-
-        @Override
-        public PreparedStatement prepareStatement(String sql, int resultSetType, int resultSetConcurrency,
-                                                  int resultSetHoldability) throws SQLException {
-            return null;
-        }
-
-        @Override
-        public CallableStatement prepareCall(String sql, int resultSetType, int resultSetConcurrency,
-                                             int resultSetHoldability) throws SQLException {
-            return null;
-        }
-
-        @Override
-        public PreparedStatement prepareStatement(String sql, int autoGeneratedKeys) throws SQLException {
-            return null;
-        }
-
-        @Override
-        public PreparedStatement prepareStatement(String sql, int[] columnIndexes) throws SQLException {
-            return null;
-        }
-
-        @Override
-        public PreparedStatement prepareStatement(String sql, String[] columnNames) throws SQLException {
-            return null;
-        }
-
-        @Override
-        public Clob createClob() throws SQLException {
-            return null;
-        }
-
-        @Override
-        public Blob createBlob() throws SQLException {
-            return null;
-        }
-
-        @Override
-        public NClob createNClob() throws SQLException {
-            return null;
-        }
-
-        @Override
-        public SQLXML createSQLXML() throws SQLException {
-            return null;
-        }
-
-        @Override
-        public boolean isValid(int timeout) throws SQLException {
-            return false;
-        }
-
-        @Override
-        public void setClientInfo(String name, String value) throws SQLClientInfoException {
-
-        }
-
-        @Override
-        public void setClientInfo(Properties properties) throws SQLClientInfoException {
-
-        }
-
-        @Override
-        public String getClientInfo(String name) throws SQLException {
-            return null;
-        }
-
-        @Override
-        public Properties getClientInfo() throws SQLException {
-            return null;
-        }
-
-        @Override
-        public Array createArrayOf(String typeName, Object[] elements) throws SQLException {
-            return null;
-        }
-
-        @Override
-        public Struct createStruct(String typeName, Object[] attributes) throws SQLException {
-            return null;
-        }
-
-        @Override
-        public void setSchema(String schema) throws SQLException {
-
-        }
-
-        @Override
-        public String getSchema() throws SQLException {
-            return null;
-        }
-
-        @Override
-        public void abort(Executor executor) throws SQLException {
-
-        }
-
-        @Override
-        public void setNetworkTimeout(Executor executor, int milliseconds) throws SQLException {
-
-        }
-
-        @Override
-        public int getNetworkTimeout() throws SQLException {
-            return 0;
-        }
-
-        @Override
-        public <T> T unwrap(Class<T> iface) throws SQLException {
-            return null;
-        }
-
-        @Override
-        public boolean isWrapperFor(Class<?> iface) throws SQLException {
-            return false;
-        }
-    };
-
-    XAResource xaResource = new XAResource() {
-        @Override
-        public void commit(Xid xid, boolean onePhase) throws XAException {
-
-        }
-
-        @Override
-        public void end(Xid xid, int flags) throws XAException {
-
-        }
-
-        @Override
-        public void forget(Xid xid) throws XAException {
-
-        }
-
-        @Override
-        public int getTransactionTimeout() throws XAException {
-            return 0;
-        }
-
-        @Override
-        public boolean isSameRM(XAResource xares) throws XAException {
-            return false;
-        }
-
-        @Override
-        public int prepare(Xid xid) throws XAException {
-            return 0;
-        }
-
-        @Override
-        public Xid[] recover(int flag) throws XAException {
-            return new Xid[0];
-        }
-
-        @Override
-        public void rollback(Xid xid) throws XAException {
-
-        }
-
-        @Override
-        public boolean setTransactionTimeout(int seconds) throws XAException {
-            return false;
-        }
-
-        @Override
-        public void start(Xid xid, int flags) throws XAException {
-
-        }
-    };
+    XAResource xaResource = TestUtils.getMockXAResource();
 
     @Test
     void commitTest1() {
+        Connection connection = TestUtils.getMockConnection(false);
         SQLTransactionContext sqlTransactionContext = new SQLTransactionContext(connection, xaResource);
         try {
             sqlTransactionContext.commit();
@@ -393,7 +48,8 @@ public class SQLTransactionContextTest {
     }
 
     @Test
-    void rollbackTest1() {
+    void rollbackTest() {
+        Connection connection = TestUtils.getMockConnection(false);
         SQLTransactionContext sqlTransactionContext = new SQLTransactionContext(connection, xaResource);
         try {
             sqlTransactionContext.rollback();
@@ -403,7 +59,30 @@ public class SQLTransactionContextTest {
     }
 
     @Test
+    void rollbackTest1() {
+        Connection connection = TestUtils.getMockConnection(true);
+        SQLTransactionContext sqlTransactionContext = new SQLTransactionContext(connection, xaResource);
+        try {
+            sqlTransactionContext.rollback();
+        } catch (Exception e) {
+            assertEquals(e.getMessage(), "transaction rollback failed:Rollback Error");
+        }
+    }
+
+    @Test
+    void closeTest() {
+        Connection connection = TestUtils.getMockConnection(false);
+        SQLTransactionContext sqlTransactionContext = new SQLTransactionContext(connection, xaResource);
+        try {
+            sqlTransactionContext.close();
+        } catch (Exception e) {
+            assertEquals(e.getMessage(), "connection close failed:Close Error");
+        }
+    }
+
+    @Test
     void closeTest1() {
+        Connection connection = TestUtils.getMockConnection(true);
         SQLTransactionContext sqlTransactionContext = new SQLTransactionContext(connection, xaResource);
         try {
             sqlTransactionContext.close();
