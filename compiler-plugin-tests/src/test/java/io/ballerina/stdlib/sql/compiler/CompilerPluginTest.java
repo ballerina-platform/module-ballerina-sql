9dbc7f662ac36304cb572c4c006b37023563aef5
Niveathika <niveathika@wso2.com>
2023-12-11 22:12:36 +0530

    Add testcase for compiler failure scenario

--- compiler-plugin-tests/src/test/java/io/ballerina/stdlib/sql/compiler/CompilerPluginTest.java
+++ compiler-plugin-tests/src/test/java/io/ballerina/stdlib/sql/compiler/CompilerPluginTest.java
@@ -57,6 +57,19 @@ public class CompilerPluginTest {
         return project.currentPackage();
     }
 
+    @Test
+    public void testCompilerErrors() {
+        Package currentPackage = loadPackage("sample3");
+        PackageCompilation compilation = currentPackage.getCompilation();
+        DiagnosticResult diagnosticResult = compilation.diagnosticResult();
+        List<Diagnostic> diagnosticErrorStream = diagnosticResult.diagnostics().stream()
+                .filter(r -> r.diagnosticInfo().severity().equals(DiagnosticSeverity.ERROR))
+                .collect(Collectors.toList());
+        long availableErrors = diagnosticErrorStream.size();
+
+        Assert.assertEquals(availableErrors, 1);
+    }
+
     @Test(enabled = false)
     public void testInvalidConnectionParamConfig() {
         Package currentPackage = loadPackage("sample1");
